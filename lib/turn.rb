98e93c2c60941f56218847297d69ffee65d93f75
sajad1993gh <sajad1993gh@gmail.com>
2019-02-01 13:34:57 -0500

    Hey

--- lib/turn.rb
+++ lib/turn.rb
@@ -12,7 +12,7 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
-#hello there !!!!XXXXX
+#hello there !!!!XXXXXNow?
 def input_to_index(user_input)
   user_input.to_i - 1
 end
