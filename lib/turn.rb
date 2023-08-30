5f0702c8a42eb77c84ffb6937334c2b589c3e882
Noah A Klem <noahaklem@gmail.com>
2023-08-29 19:39:25 -0600

    valid_move updated

--- lib/turn.rb
+++ lib/turn.rb
@@ -8,10 +8,14 @@ def display_board(board)
     puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+def input_to_index(user_input)
+    user_input = user_input.to_i - 1
+end
+
 def position_taken?(board, index)
     (board[index] == nil || board[index] == "" || board[index] == " ") ? true : false
 end
 
 def valid_move?(board, index)
-    
+    position_taken?(board, index) & index.between?(0, 8) ? true : false
 end
